47c14e0cc52955d7539dc6b2fddbf963576be041
MarkoMalezic <malezic15@gmail.com>
2026-04-22 18:17:01 +0200

    Initial commit with the fully implemented classes

---
+++ src/qiskit_omelyan/__init__.py
@@ -0,0 +1,28 @@
+from .omelyan_trotter import OmelyanTrotter
+from .omelyan_schemes import (
+    Leapfrog2,
+    Omelyan2,
+    Forest_Ruth4,
+    Omelyan4,
+    Malezic_Ostmeyer4,
+    Yoshida6,
+    Blanes_Moan6,
+    Malezic_Ostmeyer6,
+    Morales8,
+    Morales10,
+)
+
+
+__all__ = [
+    "OmelyanTrotter",
+    "Leapfrog2",
+    "Omelyan2",
+    "Forest_Ruth4",
+    "Omelyan4",
+    "Malezic_Ostmeyer4",
+    "Yoshida6",
+    "Blanes_Moan6",
+    "Malezic_Ostmeyer6",
+    "Morales8",
+    "Morales10"
+]
